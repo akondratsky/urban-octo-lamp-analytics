2aaa9048e508a71cf1a1496d975d8d02ec4dd04f
Aleksandr Kondratskii <aleksandr.kondratskii@macys.com>
2023-02-07 15:16:39 +0100

    tag manager

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          id="learn-react-link"
         >
           Learn React
         </a>
